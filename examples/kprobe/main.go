fd4d48cdff329a5b2b895587b8b08d98925591e8
Mauricio Vásquez <mauricio@kinvolk.io>
2021-07-27 09:10:50 -0500

    examples/kprobe: Disable map
    Steps to avoid the loader creating a map that won't be really used
    in the eBPF program.
    
    - set global variable to 0
    - create dummy map (it can be reuse across many eBPF programs if needed)
    - replace map by using RewriteMaps()
    
    That's it!

--- examples/kprobe/main.go
+++ examples/kprobe/main.go
@@ -11,13 +11,19 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"github.com/cilium/ebpf/link"
 	"golang.org/x/sys/unix"
+
+	"C"
+)
+import (
+	"fmt"
+
+	"github.com/cilium/ebpf"
 )
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang-11 KProbeExample ./bpf/kprobe_example.c -- -I../headers
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang-10 KProbeExample ./bpf/kprobe_example.c -- -I../headers
 
 const mapKey uint32 = 0
 
@@ -39,18 +45,55 @@ func main() {
 		log.Fatalf("failed to set temporary rlimit: %v", err)
 	}
 
-	// Load pre-compiled programs and maps into the kernel.
-	objs := KProbeExampleObjects{}
-	if err := LoadKProbeExampleObjects(&objs, nil); err != nil {
+	spec, err := LoadKProbeExample()
+	if err != nil {
 		log.Fatalf("loading objects: %v", err)
 	}
-	defer objs.Close()
+
+	// set global variable to avoid using map
+	constants := map[string]interface{}{}
+	constants["map_enabled"] = C.int(0)
+
+	if err := spec.RewriteConstants(constants); err != nil {
+		log.Fatalf("rewriting constants: %v", err)
+	}
+
+	// Create dummy eBPF map to replace the one we aren't going to use.
+	dummypec := &ebpf.MapSpec{
+		Name:       "dummy",
+		Type:       ebpf.Array,
+		KeySize:    4,
+		ValueSize:  4,
+		MaxEntries: 1,
+	}
+	dummy, err := ebpf.NewMapWithOptions(dummypec, ebpf.MapOptions{})
+	if err != nil {
+		log.Fatalf("error creating dummy map: %s", err)
+	}
+
+	// replace map we won't use by dummy map
+	maps := map[string]*ebpf.Map{}
+	maps["kprobe_map"] = dummy
+
+	if err := spec.RewriteMaps(maps); err != nil {
+		log.Fatalf("rewriting maps: %v", err)
+	}
+
+	coll, err := ebpf.NewCollection(spec)
+	if err != nil {
+		log.Fatalf("loading objects: %v", err)
+	}
 
 	// Open a Kprobe at the entry point of the kernel function and attach the
 	// pre-compiled program. Each time the kernel function enters, the program
 	// will increment the execution counter by 1. The read loop below polls this
 	// map value once per second.
-	kp, err := link.Kprobe(fn, objs.KprobeExecve)
+
+	for name, _ := range coll.Programs {
+		fmt.Printf("name is %s\n", name)
+	}
+
+	kp, err := link.Kprobe(fn, coll.Programs["kprobe_execve"] /*objs.KprobeExecve*/)
 	if err != nil {
 		log.Fatalf("opening kprobe: %s", err)
 	}
@@ -58,18 +101,18 @@ func main() {
 
 	// Read loop reporting the total amount of times the kernel
 	// function was entered, once per second.
-	ticker := time.NewTicker(1 * time.Second)
+	//ticker := time.NewTicker(1 * time.Second)
 
 	log.Println("Waiting for events..")
 
 	for {
 		select {
-		case <-ticker.C:
-			var value uint64
-			if err := objs.KprobeMap.Lookup(mapKey, &value); err != nil {
-				log.Fatalf("reading map: %v", err)
-			}
-			log.Printf("%s called %d times\n", fn, value)
+		//case <-ticker.C:
+		//	var value uint64
+		//	if err := objs.KprobeMap.Lookup(mapKey, &value); err != nil {
+		//		log.Fatalf("reading map: %v", err)
+		//	}
+		//	log.Printf("%s called %d times\n", fn, value)
 		case <-stopper:
 			return
 		}
