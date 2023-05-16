79c27e74ea6ac9c840613ab279d0401e63ffa972
Mauricio Vásquez <mauriciov@microsoft.com>
2023-05-16 17:16:22 -0500

    examples: use bpfdpath

--- examples/kprobe/main.go
+++ examples/kprobe/main.go
@@ -5,23 +5,21 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
-	"time"
+	"os"
+	"os/signal"
 
+	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
 	"github.com/cilium/ebpf/rlimit"
 )
 
 // $BPF_CLANG and $BPF_CFLAGS are set by the Makefile.
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS bpf kprobe.c -- -I../headers
-
-const mapKey uint32 = 0
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -target $TARGET -cflags $BPF_CFLAGS bpf kprobe.c -- -I../headers
 
 func main() {
-
-	// Name of the kernel function to trace.
-	fn := "sys_execve"
-
 	// Allow the current process to lock memory for eBPF resources.
 	if err := rlimit.RemoveMemlock(); err != nil {
 		log.Fatal(err)
@@ -29,33 +27,28 @@ func main() {
 
 	// Load pre-compiled programs and maps into the kernel.
 	objs := bpfObjects{}
+
 	if err := loadBpfObjects(&objs, nil); err != nil {
+		var ve *ebpf.VerifierError
+		if errors.As(err, &ve) {
+			fmt.Printf("error: %+v\n", ve)
+		}
+
 		log.Fatalf("loading objects: %v", err)
 	}
 	defer objs.Close()
 
-	// Open a Kprobe at the entry point of the kernel function and attach the
-	// pre-compiled program. Each time the kernel function enters, the program
-	// will increment the execution counter by 1. The read loop below polls this
-	// map value once per second.
-	kp, err := link.Kprobe(fn, objs.KprobeExecve, nil)
+	link, err := link.AttachTracing(link.TracingOptions{
+		Program: objs.bpfPrograms.SecurityFileOpen,
+	})
 	if err != nil {
-		log.Fatalf("opening kprobe: %s", err)
+		log.Fatal(err)
 	}
-	defer kp.Close()
+	defer link.Close()
 
-	// Read loop reporting the total amount of times the kernel
-	// function was entered, once per second.
-	ticker := time.NewTicker(1 * time.Second)
-	defer ticker.Stop()
+	// wait for ctrl-c
 
-	log.Println("Waiting for events..")
-
-	for range ticker.C {
-		var value uint64
-		if err := objs.KprobeMap.Lookup(mapKey, &value); err != nil {
-			log.Fatalf("reading map: %v", err)
-		}
-		log.Printf("%s called %d times\n", fn, value)
-	}
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
 }
